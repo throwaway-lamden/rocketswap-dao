25abf3f2797bcb147f4457288aa0d2ba10e42880
test_test <test_test@noreply.localhost>
2020-12-14 20:10:57 +0000

    Update 'custom_contract.py'

--- custom_contract.py
+++ custom_contract.py
@@ -1,7 +1,7 @@
 state = Variable()
 @export
 def run(function: str, kwargs: dict):
-    if function == set_var:
+    if function == "set_var":
         set_state(kwargs)
 def set_state(value: str):
     assert ctx.caller == "sc", "Only the parent contract can call this contract"
