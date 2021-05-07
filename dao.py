3c2a022ad5e05b7a22425e5be249023b56c246bd
throwaway-lamden <71742430+throwaway-lamden@users.noreply.github.com>
2021-05-06 20:20:25 -0400

    Added self emitting tokens and state changes

--- dao.py
+++ dao.py
@@ -1,6 +1,6 @@
 assert False, 'Do not deploy!'
 
-# AMM GOVERNANCE CONTRACT v0.2
+# AMM GOVERNANCE CONTRACT v0.2.1
 # Basic tests have not been completed. DO NOT DEPLOY.
 # This code has not been audited.
 import rswp
@@ -10,8 +10,6 @@ amm_token = rswp
 proposal_details = Hash(default_value=0)
 total_supply = Variable()
 proposal_id = Variable()
-# minimum_proposal_duration = Variable()
-# required_approval_percentage = Variable()
 state = Hash(default_value=0)
 balances = Hash(default_value=0)
 
@@ -19,11 +17,20 @@ balances = Hash(default_value=0)
 def seed():
     total_supply.set(0) 
     proposal_id.set(0)
+    state['start_time'] = now
+    state['start_rate'] = 1
+    
+    state['rate_of_increase'] = 1.0000000510010585 # 500% per year - rate is for vRSWP emission
     state['minimum_proposal_duration'] = 0 # Number is in days
     state['required_approval_percentage'] = 0.5 # Keep this at 50%, unless there are special circumstances
     state['minimum_quorum'] = 0.05 # Set minimum amount of votes needed
 
-    state['total_token_supply'] = 120000000 # May not be accurate
+    state['total_token_supply'] = 120000000 # Placeholder value, please double check before deployment
+    
+@export # this can be made not a public function
+def get_timestamp():
+    td = now - start_time
+    return td.seconds
     
 @export
 def create_proposal(proposal_type: str, proposal_args: list, description: str, voting_time_in_days: int): #Transfer tokens held by the AMM treasury here
@@ -59,6 +66,10 @@ def vote(p_id: int, amount: float, decision: bool): #Vote here
     proposal_details[p_id, ctx.caller] += amount
     proposal_details[p_id, ctx.caller, "decision"] = decision
     
+    token_amount = get_timestamp() ** state["rate_of_increase"]
+    proposal_details[p_id, ctx.caller, "issued_tokens"] += amount
+    balances[ctx.caller] += token_amount
+    
     amm_token.transfer_from(to=ctx.this, amount=amount, main_account=ctx.caller)
     
 @export
@@ -75,12 +86,16 @@ def withdraw_vote(p_id: int): #Vote here
     proposal_details[p_id, ctx.caller] = 0
     proposal_details[p_id, ctx.caller, "decision"] = 0
     
+    assert balances[ctx.caller] >= proposal_details[p_id, ctx.caller, "issued_tokens"], 'Not enough vTokens!' 
+    balances[ctx.caller] -= token_amount
+    proposal_details[p_id, ctx.caller, "issued_tokens"] = 0
+    
     amm_token.transfer(to=ctx.caller, amount=amount)
     
 @export
 def determine_results(p_id: int): #Vote resolution takes place here
     assert (proposal_details[p_id, "time"] + datetime.timedelta(days=1) * (proposal_details[p_id, "duration"])) <= now, "Proposal not over!" # Checks if proposal has concluded - TODO: Make sure this works
-    assert proposal_details[p_id, "resolved"] is not True, "Proposal already resolved" #Checks that the proposal has not been resolved before - can be replaced with `is False`
+    assert proposal_details[p_id, "resolved"] is not True, "Proposal already resolved" # Checks that the proposal has not been resolved before - can be replaced with `is False`
     
     assert p_id < proposal_id.get() # Checks proposal exists
     
@@ -91,6 +106,8 @@ def determine_results(p_id: int): #Vote resolution takes place here
         quorum -= amm_token.balance_of(x)
         
     if proposal_details[p_id, "votes", True] / (proposal_details[p_id, "votes", True] + proposal_details[p_id, "votes", False]) > state['minimum_quorum'] and proposal_details[p_id, "votes", True] >= (quorum * state['minimum_quorum']): #Checks that the approval percentage of the votes has been reached (% of total votes)
+        proposal_details[p_id, "result"] = True
+        
         if proposal_details[p_id, "type"] == "transfer": 
             t_c = importlib.import_module(proposal_details[p_id, "args"][0])
             t_c.transfer(amount=proposal_details[p_id, "args"][1], to=proposal_details[p_id, "args"][2])
@@ -103,10 +120,22 @@ def determine_results(p_id: int): #Vote resolution takes place here
             contract = importlib.import_module(proposal_details[p_id, "args"].pop(0))
             contract.run(args=proposal_details[p_id, "args"])
             
-        elif proposal_details[p_id, "type"] == "set_state":
-            state[proposal_details[p_id, "args"][0]] = proposal_details[p_id, "state"][1]
+        elif proposal_details[p_id, "type"] == "set_internal_state":
+            if proposal_details[p_id, "args"][0] == "rate_of_increase":
+                state['start_rate'] = state['start_rate'] ** state['rate_of_increase']
+                state['start_time'] = now
+                
+                state['rate_of_increase'] = proposal_details[p_id, "args"][1]
+                
+            else:
+                state[proposal_details[p_id, "args"][0]] = proposal_details[p_id, "args"][1]
             
-        proposal_details[p_id, "result"] = True
+        elif proposal_details[p_id, "type"] == "set_external_state":
+            state[proposal_details[p_id, "args"][0]] = proposal_details[p_id, "args"][1]
+            
+        else:
+            return True, proposal_details[p_id, "type"]
+            
         return True
     else:
         proposal_details[p_id, "result"] = False
