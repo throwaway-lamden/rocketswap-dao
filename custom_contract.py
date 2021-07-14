a4ebcba26c081fd40143abd54aac84da5ec39928
throwaway-lamden <71742430+throwaway-lamden@users.noreply.github.com>
2021-07-14 12:38:13 -0400

    Update code to be more consistent with dao.py style

--- custom_contract.py
+++ custom_contract.py
@@ -1,10 +1,15 @@
-#Major WIP, only a proof in concept right now
-#Do not use as-is
+# Major WIP, only a proof in concept right now
+# Do not use as-is
+
 state = Variable()
+
 @export
 def run(function: str, kwargs: dict):
+    assert ctx.caller == "sc", "Only the parent contract can call this contract"
+    
     if function == "set_var":
         set_state(kwargs)
+        
 def set_state(value: dict):
-    assert ctx.caller == "sc", "Only the parent contract can call this contract"
     state.set(value)
+    return True
